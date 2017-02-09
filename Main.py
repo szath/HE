5031bffa8b91cfbbdea0b96c4e9314a755839773
szath <guillermo.otero@gmail.com>
2017-02-09 11:01:13 -0300

    update1

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+from tkinter import *
 
+top = Tk()
+
+opt = Menu(top, activebackground="#CCCCCCFFF", bg="#700000000", title="CAJA", fg="#900900900", activeforeground="#000000700")
+fopt = Menu(opt, tearoff=0, bg="white", fg="black", activebackground="#B11ECCFFF", activeforeground="black")
+fopt.add_command(label="Abrir", command=None)
+fopt.add_command(label="Imprimir", command=None)
+opt.add_cascade(label="Caja", menu=fopt)
+
+top.iconbitmap('he_2016_logo.ico')
+top.title("Hockey Equipment")
+top.geometry("1600x900")
+
+top.configure(bg="#600600600", menu=opt)
+
+top.mainloop()
