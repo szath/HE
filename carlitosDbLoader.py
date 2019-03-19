55f2bf0a966cecf915a12d38346263a370e1018a
Iszathi <guillermo.otero@gmail.com>
2019-03-19 15:42:20 -0300

    Update3
    nm, trying panda

--- carlitosDbLoader.py
+++ carlitosDbLoader.py
@@ -1,7 +1,18 @@
 import sqlite3
 from sqlite3 import Error
 import random
+import pandas as pd
 
 conn = sqlite3.connect('hetest.db') #connects to the db in the working folder
 cur = conn.cursor() #creates db cursor
 
+"""f = open("datosCarlitosGdrive.txt", "r")
+myList = []
+for line in f:
+    myList.append(line)
+print(myList)"""
+
+df = pd.read_csv("datosCarlitosGdrive.txt")
+print(df)
+
+currentLine = 0
